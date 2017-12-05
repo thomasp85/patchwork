1283979f5787605e94551b3022f7bf1639ec5451
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-12-05 10:41:27 +0100

    Add plot spacer

---
+++ R/plot_spacer.R
@@ -0,0 +1,23 @@
+#' Add a completely blank area
+#'
+#' This simple wrapper creates an empty transparant plot that can be added to
+#' push your other plots apart
+#'
+#' @return A `ggplot` object containing an empty plot
+#'
+#' @importFrom ggplot2 ggplot theme_void theme element_rect
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' p1 <- ggplot(mtcars) + geom_point(aes(mpg, disp))
+#' p2 <- ggplot(mtcars) + geom_boxplot(aes(gear, disp, group = gear))
+#'
+#' p1 + plot_spacer() + p2
+#'
+plot_spacer <- function() {
+  ggplot() +
+    theme_void() +
+    theme(plot.background = element_rect(fill = NA, colour = NA))
+}
