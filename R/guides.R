ffcdfd013a58520a12ad0b2a04d84cdd66655b0a
yun <yunyunpp96@outlook.com>
2024-09-06 18:35:44 +0800

    fix `Guides` not in old ggplot2 version

--- R/guides.R
+++ R/guides.R
@@ -139,8 +139,10 @@ assemble_guides <- function(guides, position, theme) {
 
   # for every position, collect all individual guides and arrange them
   # into a guide box which will be inserted into the main gtable
-  Guides <- getFromNamespace("Guides", "ggplot2")
-  package_box <- .subset2(Guides, "package_box") %||% package_box
+  package_box <- tryCatch(
+    .subset2(getFromNamespace("Guides", "ggplot2"), "package_box"),
+    error = function(cnd) package_box
+  )
   package_box(guides, position, theme)
 }
 
